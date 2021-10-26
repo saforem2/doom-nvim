118026832c52377d546bdf7d11b9206c20f6f3a4
Sam Foreman <saforem2@gmail.com>
2021-10-26 09:16:25 -0500

    Updates to `doom_userplugins.lua`

--- doom_userplugins.lua
+++ doom_userplugins.lua
@@ -16,7 +16,101 @@ local M = {}
 
 M.source = debug.getinfo(1, "S").source:sub(2)
 
-M.plugins = {}
+M.plugins = {
+  { 'ActivityWatch/aw-watcher-vim' },
+  { 'wakatime/vim-wakatime' },
+  { 'ap/vim-css-color' },
+  { 'mbbill/undotree' },
+  {
+    'jakewvincent/texmagic.nvim',
+    config = function()
+      require('texmagic').setup({
+        engines = {
+          pdflatex = {
+            -- This has the same name as a default engine but would be preferred
+            -- over the same-name default if defined
+            executable = "latexmk",
+            args = {
+              "-pdflatex",
+              "-interaction=nonstopmode",
+              "-synctex=1",
+              "-outdir=.build",
+              "-pv",
+              "%f"
+            },
+            isContinuous = true
+          },
+          lualatex = {
+            -- This is *not* one of the defaults, but it can be called via
+            -- magic comment if defined here
+            executable = "latexmk",
+            args = {
+              "-pdflua",
+              "-interaction=nonstopmode",
+              "-synctex=1",
+              "-pv",
+              "%f"
+            },
+            isContinuous = true
+          }
+        }
+      })
+    end,
+  },
+  { 'ellisonleao/glow.nvim' },
+  {
+    'folke/lsp-colors.nvim',
+    config = function()
+      require("lsp-colors").setup({
+        Error = "#F20052",
+        Warning = "#FFFF00",
+        Information = "#00CCFF",
+        Hint = "#63FF51",
+      })
+    end,
+  },
+  {
+    'glepnir/lspsaga.nvim',
+    requires = { 'neovim/nvim-lspconfig' },
+  },
+  { 'wfxr/code-minimap' },
+  { 'AndrewRadev/splitjoin.vim' },
+  { 'sainnhe/sonokai' },
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSHighlightCapturesUnderCursor",
+  },
+  { 'kosayoda/nvim-lightbulb' },
+  { 'lervag/vimtex' },
+  --[[ {
+  	'nvim-treesitter/nvim-treesitter-text-objects',
+  	requires = { 'nvim-treesitter/nvim-treesitter' },
+  	config = function()
+  		require('nvim-treesitter.configs').setup({
+  			textobjects = {
+  				lsp_interop = {
+  					enable = true,
+  					border = 'none',
+  					peek_definition_code = {
+							["<leader>df"] = "@function.outer",
+							["<leader>dF"] = "@class.outer",
+						}
+					}
+				}
+			})
+		end,
+	}, ]]
+  -- { 'hrsh7th/cmp-nvim-lsp' },
+  --[[ {
+    'hrsh7th/nvim-cmp',
+    -- config = require("custom.plugin.cmp"),
+    config = function()
+      require('custom.plugin.cmp')
+    end,
+    after = "nvim-lspconfig",
+  }, ]]
+  -- { 'saadparwaiz1/cmp_luasnip', },
+}
 
 return M
 
