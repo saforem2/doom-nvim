4570499362a2921cbee4db2b3aa55e138ba73931
Sam Foreman <saforem2@gmail.com>
2021-12-23 08:31:56 -0600

    Updates to `lua/doom/modules/config/doom-luasnip.lua`

--- lua/doom/modules/config/doom-luasnip.lua
+++ lua/doom/modules/config/doom-luasnip.lua
@@ -5,23 +5,24 @@ return function()
   luasnip.config.set_config({
     history = true,
     -- Update more often, :h events for more info.
-    updateevents = "TextChanged,TextChangedI",
+    -- updatevents = "InsertLeave",
+    updateevents = "TextChanged,TextChanged",
   })
 
   require("luasnip/loaders/from_vscode").load()
 
   --- <tab> to jump to next snippet's placeholder
   local function on_tab()
-    return luasnip.jump(1) and "" or mappings.t("<Tab>")
+    return luasnip.jump(1) and "" or mappings.t("<C-Tab>")
   end
 
   --- <s-tab> to jump to next snippet's placeholder
   local function on_s_tab()
-    return luasnip.jump(-1) and "" or mappings.t("<S-Tab>")
+    return luasnip.jump(-1) and "" or mappings.t("<C-S-Tab>")
   end
 
-  mappings.imap("<Tab>", on_tab, { expr = true })
-  mappings.smap("<Tab>", on_tab, { expr = true })
-  mappings.imap("<S-Tab>", on_s_tab, { expr = true })
-  mappings.smap("<S-Tab>", on_s_tab, { expr = true })
+  mappings.imap("<C-Tab>", on_tab, { expr = true })
+  mappings.smap("<C-Tab>", on_tab, { expr = true })
+  mappings.imap("<C-S-Tab>", on_s_tab, { expr = true })
+  mappings.smap("<C-S-Tab>", on_s_tab, { expr = true })
 end
